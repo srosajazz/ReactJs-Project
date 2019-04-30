b8d42a54f4823f122b405dc22353344c3826f911
Sergio Rosa <scarlosjazz@gmail.com>
2019-04-29 21:14:34 -0400

    Creating First React Component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-const sum = (a, b) => a + b;
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
 
-alert(sum(20, 3));
+//reuse alredy use insed of another one
+class Button extends Component {
+  render() {
+    return <a href="">Send</a>;
+  }
+}
+
+//Create a component
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Sergio</h1>
+        <Button />
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
