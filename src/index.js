fec3c5eefa7a2ec8a344801fab0d609864358e53
Sergio Rosa <scarlosjazz@gmail.com>
2019-04-30 08:38:03 -0400

    Passing properties/Children

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import { render } from "react-dom";
 //reuse alredy use insed of another one
 class Button extends Component {
   render() {
-    return <a href="">Send</a>;
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
   }
 }
 
 //Create a component
 class App extends Component {
+  handleClick() {
+    alert("button clicked!");
+  }
   render() {
     return (
       <Fragment>
         <h1>Hello Sergio</h1>
-        <Button />
+        {/* <Button title="Jazzz" /> Proprity */}
+        <Button onClick={this.handleClick}>Jazzzzzz</Button>
       </Fragment>
     );
   }
